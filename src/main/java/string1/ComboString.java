aa6c192d7afa57a084f9728b44744f019eed58e4
emelebruergin <e.ebruergin@hotmail.com>
2023-04-30 21:09:28 +0300

    combostring

---
+++ src/main/java/string1/ComboString.java
@@ -0,0 +1,27 @@
+package string1;
+
+public class ComboString {
+    /*
+    Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+
+
+        comboString("Hello", "hi") → "hiHellohi"
+        comboString("hi", "Hello") → "hiHellohi"
+        comboString("aaa", "b") → "baaab"
+     */
+    public static void main(String[] args) {
+        String a="Hello";
+        String b="hi";
+        System.out.println(comboString(a, b));
+    }
+    public static String comboString(String a, String b) {
+        if(a.length()>b.length()){
+            return b+a+b;
+        }else {
+            return a+b+a;
+        }
+
+    }
+
+
+}
